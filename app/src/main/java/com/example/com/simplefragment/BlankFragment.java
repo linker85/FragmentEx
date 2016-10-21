3b1ce547d29b3488ccbe42408f9edd191c63e103
linker85 <linker85@hotmail.com>
2016-10-21 16:03:35 -0400

    Simple fragment example

---
+++ app/src/main/java/com/example/com/simplefragment/BlankFragment.java
@@ -0,0 +1,30 @@
+package com.example.com.simplefragment;
+
+
+import android.os.Bundle;
+// Always select the one that supports before 24.1
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BlankFragment extends Fragment {
+
+
+    public BlankFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_blank, container, false);
+    }
+
+}
